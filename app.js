5bf0fa3b79b2fcbccc9524832449f803307e936d
zhangcj <602173141@qq.com>
2026-01-22 12:26:55 +0800

    fix: 修复环境ID硬编码问题

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ App({
   onLaunch() {
     // 初始化云开发环境
     wx.cloud.init({
-      env: 'cloud1-5gcmcrpr162d6ee6',
+      env: cloud.DYNAMIC_CURRENT_ENV,
       traceUser: true
     })
-    
+
     console.log('Time in a Teacup Mini Program launched')
   },
   globalData: {
